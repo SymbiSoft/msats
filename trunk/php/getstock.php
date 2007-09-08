f36f41028d3614136097db94bb677faca06fad78
01soft <01soft@430ebab5-3035-0410-bc3c-4788c7a0d4e6>
2007-09-08 08:18:33 +0000

    n

--- trunk/php/getstock.php
+++ trunk/php/getstock.php
@@ -54,6 +54,63 @@ while (!feof($fd))
 fclose($fd); 
 } 
 
+function getSohuQuote($stockSymbol = "000001") 
+{ 
+if (!$targetURL) $targetURL = "http://stock.business.sohu.com/stock_image/realtime_table.php?code=$stockSymbol"; //设定要抓取的URL目标 
+$fd = fopen("$targetURL", "r"); 
+$stopExtract = 0; 
+$startExtract = 0; 
+while (!feof($fd)) 
+{ 
+	$buffer = fgets($fd, 4096); 
+	
+	if (!$startExtract)
+	{
+		if (strstr($buffer, "相关")) 
+		{ 
+			$startExtract = 1; 
+		}
+	}
+ 
+	if ($startExtract && !$stopExtract) 
+	{ 
+		$buffer = str_replace("</td>", "|", $buffer); 
+		$buffer = str_replace("&nbsp;", "", $buffer); 
+		$buffer = trim(strip_tags($buffer)); 
+		if (strstr($buffer, "Ｂ")) 
+		{ 
+			$stopExtract = 1; 
+		} 
+		$capturedHTML .= $buffer; 
+	} 
+	if ($startExtract && strstr($buffer, "Ｂ")) 
+	{ 
+		$stopExtract = 1;
+		$capturedHTML = str_replace("相关", "", $capturedHTML); 
+		$capturedHTML = str_replace("ＫＦＨＴＮＢ||", "", $capturedHTML); 
+		$capturedHTML = str_replace("|Ｋ- Ｋ线走势图Ｆ- 重要财务指标Ｈ- 历史成交明细Ｔ- 技术指标Ｎ- 最新资讯Ｂ- 网友留言", "", $capturedHTML);  
+		
+		
+		$parts = split("\|",$capturedHTML);
+		if (count($parts)>2)
+   	{
+   		$parts[2]="";
+   		$capturedHTML="";
+   		$capturedHTML=$capturedHTML.$parts[6]."|".$parts[3];
+   		//for ($i=1;$i<count($parts)-1;$i++) 
+   		//{
+   		//		$capturedHTML=$capturedHTML.$parts[$i]."|";		
+     	//}
+   }
+
+		echo $capturedHTML; 
+		break; 
+	} 
+	$lineCount++; 
+} 
+fclose($fd); 
+} 
+
 //以下为抓取的一个例子 
 //$symbols = array('000001','000002','000003','000004' ); 
 //$symbolCount = count($symbols); 
@@ -61,12 +118,16 @@ fclose($fd);
 //{ 
 //	get163Quote("$symbols[$i]");
 	$codelist=$_GET['code'];
-	$parts = split(";",$codelist);
+	$parts = split("\|",$codelist);
 	for ($i=0;$i<count($parts);$i++)
 	{
 		$code=$parts[$i];
 		get163Quote("$code");
-		echo ";";
+		if($i!=count($parts)-1)
+		{
+			echo ";";
+		}
 	} 
 //} 
 ?> 
+
